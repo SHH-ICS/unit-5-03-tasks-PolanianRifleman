af0753f3ae3fa62a549e37adf34990a2ce4c19d7
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-05-17 04:14:10 +0000

    finished( too late to do this) i'm going to bed

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,23 @@
-# Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
-# This will be six lines, and then store the questions in the file questions.txt.
+import json
+
+q = input("Input the multiple choice question: ")
+a = input("Input option A: ")
+b = input("Input option B: ")
+c = input("Input option C: ")
+d = input("Input option D: ")
+ca = input("Input the letter of the correct answer: ")
+
+question_data = {
+    "question": q,
+    "answers": {
+        "A": a,
+        "B": b,
+        "C": c,
+        "D": d
+    },
+    "correct_answer": ca
+}
+
+with open("questions.json", "a") as file:
+    json.dump(question_data, file)
+    file.write("\n")
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,35 @@
-# Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
-# The program will keep score of the number of questions answered correctly.
+import json
+
+with open("questions.txt", "r") as file:
+    q = file.readlines()
+
+score = 0
+
+for i in range(0, len(q), 7):
+    question_data = {
+        "question": q[i].strip(),
+        "answers": {
+            "A": q[i+1].strip(),
+            "B": q[i+2].strip(),
+            "C": q[i+3].strip(),
+            "D": q[i+4].strip()
+        },
+        "correct_answer": q[i+5].strip()
+    }
+
+    print("\nQuestion:", question_data["question"])
+    print("A:", question_data["answers"]["A"])
+    print("B:", question_data["answers"]["B"])
+    print("C:", question_data["answers"]["C"])
+    print("D:", question_data["answers"]["D"])
+
+    user_answer = input("Enter your answer (A/B/C/D): ")
+
+    if user_answer.upper() == question_data["correct_answer"]:
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+print("\nQuiz completed!")
+print("Your score:", score, "out of", len(q) // 7)
